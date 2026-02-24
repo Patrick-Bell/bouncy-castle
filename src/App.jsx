c1bf5489b59db809528e8b644f0cfca39c93eb23
Patrick Bell <patrick@Patricks-Laptop.local>
2026-02-24 13:41:43 +0000

    adding al changes to contact page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import FAQPage from './components/FAQ/FAQ'
 import SpeedDial from './components/SpeedDial/SpeedDial'
 import Products from './components/Catalogue/Products'
 import DynamicProductPage from './components/Catalogue/DynamicProductPage'
+import Contact from './components/Contact/Contact'
 
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
       <Route path='/frequently-asked-questions' element={<FAQPage />} />
       <Route path='/bouncy-castles' element={<Products />} />
       <Route path='/bouncy-castle/:id' element={<DynamicProductPage />} />
+      <Route path='/contact' element={<Contact />} />
       </Routes>
 
       <SpeedDial />
